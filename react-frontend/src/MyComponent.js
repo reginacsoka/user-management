20f6dd53c31a50082826bcd781b48a3f5e5c0ecf
regina <reginacsoka@gmial.com>
2021-03-14 21:15:20 +0200

    init react-frontend

---
+++ react-frontend/src/MyComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  title: PropTypes.any
+};
+
+const defaultProps = {
+  name: 'Default name of App'
+};
+
+class MyComponent extends Component{
+  
+  shouldComponentUpdate(nextProps, nextState){
+      return true;
+  }
+
+
+  render() {
+    const { title, name, onClick } = this.props;
+
+    return(
+    <div className="component">
+      <h1>Title: {title}</h1>
+      <h2>Name: {name}</h2>
+      <div onClick={onClick}>Click me!</div>
+    </div>
+    );
+  }
+}
+
+MyComponent.propTypes = propTypes;
+MyComponent.defaultProps = defaultProps;
+
+export default MyComponent;
